2a08a51c81ba154022f31ad901f340192f7bbf84
miwaark0810 <a.niwa80@outlook.jp>
2020-12-22 16:01:25 +0900

    編集機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,23 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+    unless @post.user_id == current_user.id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+     if @post.update(post_params)
+      redirect_to post_path
+     else
+      render :edit
+     end
+  end
+  
+
   def post_params
     params.require(:post).permit(:subject, :title, :text, :image).merge(user_id: current_user.id)
   end
