f9311a843ca61de5d365b556f1ee7794294dfd50
miwaark0810 <a.niwa80@outlook.jp>
2020-12-22 17:19:34 +0900

    postモデル単体テストコードの記述

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @post = FactoryBot.build(:post)
+    end
+
+    it 'すべての項目が存在すれば保存できること' do
+      expect(@post).to be_valid
+    end
+
+    it 'imageが空では登録できないこと' do
+      @post.image = nil
+      @post.valid?
+      expect(@post.errors.full_messages).to include("Image can't be blank")
+    end
+
+    it 'subjectが空では登録できないこと' do
+      @post.subject = nil
+      @post.valid?
+      expect(@post.errors.full_messages).to include("Subject can't be blank")
+    end
+    
+    it 'titleが空では登録できないこと' do
+      @post.title = nil
+      @post.valid?
+      expect(@post.errors.full_messages).to include("Title can't be blank")
+    end
+
+    it 'textが空では登録できないこと' do
+      @post.text = nil
+      @post.valid?
+      expect(@post.errors.full_messages).to include("Text can't be blank")
+    end
+
+    it 'userが紐付いていないと保存できないこと' do
+      @post.user = nil
+      @post.valid?
+      expect(@post.errors.full_messages).to include('User must exist')
+    end
+  end
 end
