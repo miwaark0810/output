3ffcdb114cfb26adbca22bcd9c96051aec1f3f4c
miwaark0810 <a.niwa80@outlook.jp>
2021-01-05 12:01:31 +0900

    回答機能の実装

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,18 @@
+class AnswersController < ApplicationController
+  def create
+    @answer = answer.new(answer_params)
+    if @answer.save
+      redirect_to question_path(@answer.question_id)
+    else
+      @question = @answer.question
+      @answers = @prototype.answers
+      render "question/show"
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:text).merge(user_id: current_user.id, question_id: params[:question_id])
+  end
+end
