a3dc4a48e995f89c1d7cd29d5a0378fb66183318
miwaark0810 <a.niwa80@outlook.jp>
2021-01-12 11:17:15 +0900

    postモデルのテストコード追加

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -27,11 +27,33 @@ RSpec.describe Post, type: :model do
       @post.valid?
       expect(@post.errors.full_messages).to include("Title can't be blank")
     end
+    
+    it 'titleが16文字以下であれば登録できること' do
+      @post.title = 'abcdefghijklmnop'
+      expect(@post).to be_valid
+    end
+    
+    it 'titleが17文字以上であれば登録できないこと' do
+      @post.title = 'abcdefghijklmnopq'
+      @post.valid?
+      expect(@post.errors.full_messages).to include('Title is too long (maximum is 16 characters)')
+    end
 
     it 'textが空では登録できないこと' do
       @post.text = nil
       @post.valid?
       expect(@post.errors.full_messages).to include("Text can't be blank")
+    end
+
+    it 'textが60文字以下であれば登録できること' do
+      @post.text = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefgh'
+      expect(@post).to be_valid
+    end
+    
+    it 'textが61文字以上であれば登録できないこと' do
+      @post.text = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghi'
+      @post.valid?
+      expect(@post.errors.full_messages).to include('Text is too long (maximum is 60 characters)')
     end
 
     it 'userが紐付いていないと保存できないこと' do
