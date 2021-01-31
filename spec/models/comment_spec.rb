ce10257b03dff39fa6652ea875648d8950db5361
miwaark0810 <a.niwa80@outlook.jp>
2021-01-31 12:20:21 +0900

    コメントモデルのテストコード記述

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @comment = FactoryBot.build(:comment)
+    end
+
+    it 'テキストが存在すれば保存できること' do
+      expect(@comment).to be_valid
+    end
+
+    it 'テキストが空では登録できないこと' do
+      @comment.text = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include("Text can't be blank")
+    end
+
+    it 'userが紐付いていないと保存できないこと' do
+      @comment.user = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include('User must exist')
+    end
+
+    it 'postが紐付いていないと保存できないこと' do
+      @comment.post = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include('Post must exist')
+    end
+  end
 end
